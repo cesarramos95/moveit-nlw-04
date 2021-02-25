a3b1f59977ed464bfae5d4b03c7f2d8273edd3bd
cesarramos95 <cesarramos.aug@gmail.com>
2021-02-25 00:25:08 -0300

    Created Context to control experience score in the application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import { CompletedChallenges } from '../components/CompletedChallenges';
-import { Countdown } from '../components/Countdown';
-import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from "../components/Profile";
-
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { ChallengeBox } from '../components/ChallengeBox';
+import { Countdown } from '../components/Countdown';
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
 
 export default function Home() {
   return (
@@ -22,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
