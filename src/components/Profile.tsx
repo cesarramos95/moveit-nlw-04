cc2a453f0aa1fb6cff277a16ce8c9eed7afe69e3
cesarramos95 <cesarramos.aug@gmail.com>
2021-02-25 21:25:13 -0300

    Create completeChallenge function

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/cesarramos95.png" alt="César Augusto"/>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   levelUp: () => void;
   startNewChallenge: () => void;
   resetChallenge: () => void;
+  completeChallenge: () => void;
 }
 
 interface ChallengeProviderProps {
@@ -48,6 +49,26 @@ export function ChallengesProvider({ children }: ChallengeProviderProps) {
     setActiveChallenge(null);
   }
 
+  function completeChallenge() {
+    if (!activeChallenge) {
+      return;
+    }
+
+    const { amount } = activeChallenge;
+
+    let finalExperience = currentExperience + amount;
+
+    if (finalExperience >= experienceToNextLevel) {
+      finalExperience = finalExperience - experienceToNextLevel;
+      levelUp();
+    }
+
+    setCurrentExperience(finalExperience);
+    setActiveChallenge(null);
+    setChallengesCompleted(challengesCompleted + 1);
+
+  }
+
   return (
     <ChallengesContext.Provider 
       value={{ 
@@ -58,7 +79,8 @@ export function ChallengesProvider({ children }: ChallengeProviderProps) {
         levelUp, 
         startNewChallenge,
         activeChallenge, 
-        resetChallenge
+        resetChallenge,
+        completeChallenge
       }}
     >
       {children}
